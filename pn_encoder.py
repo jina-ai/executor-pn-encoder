c370becc4c629254fc1c38899668516bd3a3158e
Alaeddine Abdessalem <alaeddine-13@live.fr>
2021-10-13 11:35:51 +0100

    feat: support gpu

--- pn_encoder.py
+++ pn_encoder.py
@@ -1,9 +1,10 @@
 from typing import Dict, Iterable
 
 import numpy as np
-from jina import Document, DocumentArray, Executor, requests
+from jina import DocumentArray, Executor, requests
 from jina.logging.logger import JinaLogger
 from jina_commons.batching import get_docs_batch_generator
+import tensorflow as tf
 
 
 from pn import get_bottleneck_model
@@ -19,16 +20,28 @@ class PNEncoder(Executor):
             ckpt_path: str = 'ckpt/ckpt_True',
             traversal_paths: Iterable[str] = ('r',),
             batch_size: int = 32,
+            device: str = '/CPU:0',
             **kwargs):
         """
         :param ckpt_path: model checkpoint file path
         :param traversal_paths: traversal path of the Documents, (e.g. 'r', 'c')
         :param batch_size: size of each batch
+        :param device: Device ('/CPU:0', '/GPU:0', '/GPU:X')
         """
         super().__init__(**kwargs)
-        self.embedding_model = get_bottleneck_model(ckpt_path=ckpt_path)
         self.batch_size = batch_size
         self.traversal_paths = traversal_paths
+
+        gpus = tf.config.experimental.list_physical_devices(device_type='GPU')
+        if 'GPU' in device:
+            gpu_index = 0 if 'GPU:' not in device else int(device.split(':')[-1])
+            if len(gpus) < gpu_index + 1:
+                raise RuntimeError(f'Device {device} not found on your system!')
+        self.device = tf.device(device)
+
+        with self.device:
+            self.embedding_model = get_bottleneck_model(ckpt_path=ckpt_path)
+
         self.logger = JinaLogger('pn-encoder')
 
     @requests(on=['/index', '/search'])
@@ -52,6 +65,7 @@ class PNEncoder(Executor):
     def _create_embeddings(self, document_batches_generator: Iterable):
         for document_batch in document_batches_generator:
             blob_batch = np.stack([d.blob for d in document_batch])
-            embedding_batch = self.embedding_model.predict(blob_batch)[-1]
+            with self.device:
+                embedding_batch = self.embedding_model.predict(blob_batch)[-1]
             for document, embedding in zip(document_batch, embedding_batch):
                 document.embedding = embedding
