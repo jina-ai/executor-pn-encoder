8df7d35900d6e48b6198c7bb3086b080dc92817e
Alaeddine Abdessalem <alaeddine-13@live.fr>
2021-10-14 00:24:59 +0100

    test: add integration test

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -1,2 +1,13 @@
-from jina import Flow
+import numpy as np
+from jina import Flow, DocumentArray, Document
 
+
+def test_flow():
+    docs = DocumentArray([Document(blob=np.random.rand(2048, 3)) for _ in range(3)])
+    with Flow.load_config('flow.yml') as flow:
+        data = flow.post(
+            on='/index', inputs=docs, return_results=True
+        )
+        for doc in data[0].docs:
+            assert doc.embedding is not None
+            assert doc.embedding.shape == (128,)
